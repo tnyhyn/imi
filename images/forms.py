63f1f8fb3e1b94dd021ec84d431bca40d697f732
TH <tnyhyn@gmail.com>
2019-10-18 21:56:18 -0400

    styling buttons and reverted upload page to plain HTML (bootstrap broke upload)

--- images/forms.py
+++ images/forms.py
@@ -5,12 +5,12 @@ from .models import Post
 
 
 class UploadImageForm(forms.ModelForm):
-    title = forms.CharField(max_length=50)
+    title = forms.CharField(max_length=50, required=False, initial='MyImage')
     private = forms.BooleanField(required=False)
     image = forms.ImageField()
 
     class Meta:
         model = Post
-        fields = ('title', 'image', 'private', )
+        fields = ('title', 'image', 'private')
 
 
