abb9044b7d4c21bbd71e7335a5b71800a7c9decd
TH <tnyhyn@gmail.com>
2019-10-19 15:03:05 -0400

    images are now uploaded with unique filenames

--- images/models.py
+++ images/models.py
@@ -1,17 +1,16 @@
+import uuid
+import os
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=20)
-    pub_date = models.DateTimeField(null=True)
-    image = models.ImageField(upload_to='img')
-    private = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+    def get_file_path(instance, filename):
+        ext = filename.split('.')[-1]
+        filename = "%s.%s" % (uuid.uuid4(), ext)
+        return os.path.join('img', filename)
 
     def save(self, *args, **kwargs):
         self.pub_date = timezone.now()
@@ -20,6 +19,16 @@ class Post(models.Model):
     def delete(self, *args, **kwargs):
         self.image.delete()
         super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+        
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(null=True)
+    image = models.ImageField(upload_to=get_file_path)
+    private = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 
 
