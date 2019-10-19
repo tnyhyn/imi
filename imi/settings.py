e7f4b049beadaf6ea38cfec41af1fbf0063607cc
TH <tnyhyn@gmail.com>
2019-10-19 19:12:22 -0400

    allowed_hosts added for prod

--- imi/settings.py
+++ imi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secret-beyond-69744.herokuapp.com', '127.0.0.1']
 
 STATIC_URL = '/static/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
