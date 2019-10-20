cfb804b6410bae622ee89c722516e9cfee732557
TH <tnyhyn@gmail.com>
2019-10-20 00:53:50 -0400

    aaaah

--- imi/settings.py
+++ imi/settings.py
@@ -84,12 +84,7 @@ WSGI_APPLICATION = 'imi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
