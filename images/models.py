dc62837dec3851aaefd34c657a16e97480b8d9ee
TH <tnyhyn@gmail.com>
2019-10-20 02:41:31 -0400

    add migration file

--- images/models.py
+++ images/models.py
@@ -22,7 +22,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+    
+    
     title = models.CharField(max_length=20)
     pub_date = models.DateTimeField(null=True)
     image = models.ImageField(upload_to=get_file_path)
