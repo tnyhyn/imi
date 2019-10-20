f11302c7dfbc19fc5213b995c10d71a45532ee35
TH <tnyhyn@gmail.com>
2019-10-19 22:30:09 -0400

    no message

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('upload/', views.upload_file, name='upload_file'),
     path('myimages/<int:pk>/', views.delete_file, name='delete_file'),
     path('register/', views.register, name='register'),
