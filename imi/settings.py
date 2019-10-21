aa49ed95d647fb05eb28325a5bcbc50ce0c948e8
TH <tnyhyn@gmail.com>
2019-10-21 01:19:04 -0400

    debug set to false for production

--- imi/settings.py
+++ imi/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "gB/(Tu#Wd,ulic[{OXWw:Xn+#7*FD>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
